697346770fd9c587474c6b2bc8aaebaaa59545cb
IlanG96 <goldlmilan@gmail.com>
2021-12-18 14:25:45 +0200

    V 1.2

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,7 +1,13 @@
-
 from Node import Node
 from GraphInterface import GraphInterface
 
+
+def v_size():
+    """
+    return the number of nodes in graph
+    :return: the number of nodes in graph
+    """
+    return len(self.nodes)
 
 
 class DiGraph:
@@ -9,15 +15,6 @@ class DiGraph:
         self.nodes = dict()
         self.num_of_edge = 0
         self.mc = 0
-
-
-
-    def v_size(self):
-        """
-        return the number of nodes in graph
-        :return: the number of nodes in graph
-        """
-        return self.nodes.__len__()
 
     def e_size(self):
         """
@@ -39,7 +36,7 @@ class DiGraph:
         :param id1:
         :return:
         """
-        if(self.nodes.get(id1) is None):
+        if self.nodes.get(id1) is None:
             raise Exception('node:{}'.format(id1))
         return self.nodes.get(self, id1).connect_in()
 
@@ -47,10 +44,9 @@ class DiGraph:
         """return a dictionary of all the nodes connected from node_id , each node is represented using a pair
         (other_node_id, weight)
         """
-        if(self.nodes.get(id1) is None):
+        if self.nodes.get(id1) is None:
             raise Exception('node:{}'.format(id1))
         return self.nodes.get(id1).connect_out
-
 
     def get_mc(self) -> int:
         """
@@ -70,13 +66,13 @@ class DiGraph:
         @return: True if the edge was added successfully, False o.w.
         Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
         """
-        raise NotImplementedError
-        if(self.nodes.get(id1) is None):
+
+        if self.nodes.get(id1) is None:
             return False
-        if(self.nodes.get(id2) is None):
+        if self.nodes.get(id2) is None:
             return False
-        self.num_of_edge+1
-        self.mc+1
+        self.num_of_edge + 1
+        self.mc + 1
         self.nodes.get(id1).add_connect_out(id2, weight)
         self.nodes.get(id2).add_connect_in(id1, weight)
         return True
@@ -89,23 +85,47 @@ class DiGraph:
         @return: True if the node was added successfully, False o.w.
         Note: if the node id already exists the node will not be added
         """
-        if(self.nodes.get(node_id) is None):
+        if node_id in self.nodes:
             return False
-        self.mc+1
+        self.mc += 1
         node = Node(node_id, pos)
         self.nodes[node_id] = node
         return True
 
     def remove_node(self, node_id: int) -> bool:
-       if(node_id not in self.nodes):
-          return False
-        node = self.nodes[node_id]
+        if node_id not in self.nodes:
+            return False
+        removed_node = self.nodes[node_id]
+        for dist in removed_node.connect_out.keys():
+            self.remove_edge(node_id, dist)
+        for source in removed_node.connect_in.keys():
+            self.remove_edge(source, node_id)
+        del self.nodes[node_id]
+        self.mc += 1
+        return True
 
+    def remove_node(self, node_id: int) -> bool:
+        """
+        Removes a node from the graph.
+        @param node_id: The node ID
+        @return: True if the node was removed successfully, False o.w.
+        Note: if the node id does not exists the function will do nothing
+        """
+        return True;
 
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        """
+        Removes an edge from the graph.
+        @param node_id1: The start node of the edge
+        @param node_id2: The end node of the edge
+        @return: True if the edge was removed successfully, False o.w.
+        Note: If such an edge does not exists the function will do nothing
+        """
+        return True
 
 # g = DiGraph()# for f in g.edges:
 #     print(f.src)
-#print(g.nodes[1].id)
+# print(g.nodes[1].id)
 # for e in edges:
 #     print(e)
 #
--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,7 +1,29 @@
-from GraphAlgoInterface import GraphAlgoInterface
+import json
 
+from GraphAlgoInterface import GraphAlgoInterface
+from GraphInterface import GraphInterface
+from DiGraph import DiGraph
 
 
 class GraphAlgo(GraphAlgoInterface):
-    def __init__(self, dg: DiGraph = None):
-        self.graph = DiGraph
+    def _init_(self, graph):
+        self.graph = graph
+
+    def get_graph(self) -> GraphInterface:
+        """
+        :return: the directed graph on which the algorithm works on.
+        """
+        self.graph
+
+    def load_from_json(self, file_name: str) -> bool:
+        """
+        Loads a graph from a json file.
+        @param file_name: The path to the json file
+        @returns True if the loading was successful, False o.w.
+        """
+        my_graph = DiGraph()
+        with open("path_to_file", 'r') as file:
+            g_dict = json.load(file)
+        "load the nodes from dict to our graph"
+        for node in g_dict['nodes']:
+            my_graph.add_node(node.id, node.pos)
